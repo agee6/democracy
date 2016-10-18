28a1031ffaa2055287bcf28e8900b565382ec3a3
Godsten Silver <geeausten@gmail.com>
2016-10-18 14:35:54 -0700

    t

--- frontend/apiUtil.js
+++ frontend/apiUtil.js
@@ -140,4 +140,21 @@ var APIUtil = {
 
 };
 
+// window.fbAsyncInit = function() {
+//   FB.init({
+//     appId      : '1069565296472933',
+//     xfbml      : true,
+//     version    : 'v2.8'
+//   });
+// };
+//
+// (function(d, s, id){
+//   var js, fjs = d.getElementsByTagName(s)[0];
+//   if (d.getElementById(id)) {return;}
+//   js = d.createElement(s); js.id = id;
+//   js.src = "//connect.facebook.net/en_US/sdk.js";
+//   fjs.parentNode.insertBefore(js, fjs);
+// }(document, 'script', 'facebook-jssdk'));
+
+
 module.exports = APIUtil;
